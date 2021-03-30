9c622a9559880cc21517dfccaef0c27c7ec7a37b
IntegralTeam <repo@sfxdx.ru>
2021-03-30 09:53:42 +0700

    Change deploy_contract.js

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,19 @@
+const MockERC20 = artifacts.require("MockERC20");
+const BlockhashStore = artifacts.require("BlockhashStore");
 const VORCoordinator = artifacts.require("VORCoordinator");
+const VORD20 = artifacts.require("VORD20");
 
 module.exports = function(deployer) {
-  deployer.deploy(VORCoordinator);
+  deployer.then(async () => {
+    const xfund = "0x0000000000000000000000000000000000000000"
+    // When deploying to mainnet, you need to change to the real address of the contract
+    // const erc20 = await deployer.deploy(MockERC20, 'xFUND', 'xFUND', web3.utils.toWei('1000000000', 'ether'));
+    // const xfund = erc20.address
+
+    const block = await deployer.deploy(BlockhashStore);
+    const vor = await deployer.deploy(VORCoordinator, xfund, block.address);
+
+    // For tests, you must substitute the correct values
+    // deployer.deploy(VORD20, vor.address, xfund, web3.utils.fromAscii('keyHash'), web3.utils.toWei('1', 'ether'));
+  });
 };
