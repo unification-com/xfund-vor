eb4ed22206ab81ed071b842b65c575bd567cee2a
Codegnosis <paul@codegnosis.co.uk>
2021-04-07 15:57:49 +0100

    oracle - store gas refund data

--- oracle/store/randomness_request.go
+++ oracle/store/randomness_request.go
@@ -6,5 +6,6 @@ type IRandomnessRequestStore interface {
 	InsertNewRequest(keyHashHex string, seedHex string, senderHex string, requestIDHex string, blockHashHex string, blockNumber uint64, transactionID string, status string) error
 	Migrate() (err error)
 	Last() (request models.IRandomnessRequestStoreModel, err error)
-	UpdateFulfillment(requestId string, fulfillTxHash string, status string, gasUsed uint64, blockNum uint64) error
+	UpdateFulfillment(requestId string, fulfillTxHash string, status string, gasUsed uint64, blockNum uint64, gasPrice uint64) error
+	UpdateGasRefund(requestId string, amountRefunded uint64, gasUsed uint64, gasPrice uint64) error
 }
