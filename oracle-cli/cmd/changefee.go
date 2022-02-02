f639ac19026b4e5c143fa263acac547dd3f84f04
Codegnosis <paul@codegnosis.co.uk>
2022-02-02 14:21:53 +0000

    oracle-cli - remove some comments

--- oracle-cli/cmd/changefee.go
+++ oracle-cli/cmd/changefee.go
@@ -63,14 +63,4 @@ var changefeeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(changefeeCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// changefeeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// changefeeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
